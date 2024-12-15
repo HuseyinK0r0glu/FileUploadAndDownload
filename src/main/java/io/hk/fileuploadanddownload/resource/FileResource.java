627bae25fa03c768a60e6c00fd9f82eda2fe7ff8
HuseyinK0r0glu <korogluh27@gmail.com>
2024-12-15 21:12:47 +0300

    download method added

--- src/main/java/io/hk/fileuploadanddownload/resource/FileResource.java
+++ src/main/java/io/hk/fileuploadanddownload/resource/FileResource.java
@@ -1,13 +1,15 @@
 package io.hk.fileuploadanddownload.resource;
 
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.StringUtils;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
@@ -16,6 +18,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static java.nio.file.StandardCopyOption.REPLACE_EXISTING;
+import static org.springframework.http.HttpHeaders.CONTENT_DISPOSITION;
 
 @RestController
 @RequestMapping("/file")
@@ -43,5 +46,17 @@ public class FileResource {
     }
 
     // define a method to download files
-
+    @GetMapping("/download/{filename}")
+    public ResponseEntity<Resource> downloadFiles(@PathVariable("filename") String filename) throws IOException {
+        Path filePath = Paths.get(DIRECTORY).toAbsolutePath().normalize().resolve(filename);
+        if(!Files.exists(filePath)){
+            throw new FileNotFoundException(filename + " was not found on the server");
+        }
+        Resource resource = new UrlResource(filePath.toUri());
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("File-Name",filename);
+        headers.add(CONTENT_DISPOSITION,"attachment;File-Name=" + resource.getFilename());
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(Files.probeContentType(filePath)))
+                .headers(headers).body(resource);
+    }
 }
